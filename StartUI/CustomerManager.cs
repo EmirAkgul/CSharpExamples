177749ad4f77e57e1579c802a2a87cac270249c4
Emir Akgul <emirakgl08@gmail.com>
2022-11-27 16:36:02 +0300

    Add project files.

---
+++ StartUI/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUI
+{
+    public class CustomerManager
+    {
+        public void GetAll(Customer customer)
+        {
+            Console.WriteLine("Müşteri Adı: "+customer.FirstName);
+            Console.WriteLine("Müşteri Soyadı: " + customer.LastName);
+        }
+
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + "Adlı müşteri eklendi.");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + "Adlı müşteri silindi.");
+        }
+    }
+}
---
+++ StartUI/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace StartUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Test";
+            customer.LastName = "Test";
+
+            CustomerManager customerManager= new CustomerManager();
+            customerManager.Add(customer);
+            customerManager.Delete(customer);
+            customerManager.GetAll(customer);
+
+            Console.ReadLine();
+        }
+    }
+}
